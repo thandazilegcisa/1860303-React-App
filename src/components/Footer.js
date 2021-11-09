c4dfb4a5d79fa679ecc85bc9f77bc000ad9b3a4e
thandazilegcisa <gcisatahn@gmail.com>
2021-11-09 21:58:56 +0200

    Footer-Done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,7 @@ class Footer extends React.Component{
                               y2="225"
                               height="25"
                               stroke="black"
-                              stroke-width="8"
+                              stroke-width="10"
                          ></line>
                          <line 
                               x1="0"
@@ -25,135 +25,47 @@ class Footer extends React.Component{
                               y2="125"
                               height="25"
                               stroke="black"
-                              stroke-width="8"
+                              stroke-width="10"
                          ></line>
                        </g>
-                       <line 
-                              x1="5"
-                              y1="225"
-                              x2="110"
-                              y2="125"
-                              height="12"
-                              stroke="black"
-                              stroke-width="3"
-                        ></line>
-                        <line 
-                              x1="215"
-                              y1="225"
-                              x2="105"
-                              y2="125"
-                              height="12"
-                              stroke="black"
-                              stroke-width="3"
-                        ></line>
-                        <line 
-                              x1="215"
-                              y1="225"
-                              x2="315"
-                              y2="125"
-                              height="12"
-                              stroke="black"
-                              stroke-width="3"
-                        ></line>
-                        <line 
-                              x1="410"
-                              y1="220"
-                              x2="310"
-                              y2="125"
-                              height="12"
-                              stroke="black"
-                              stroke-width="3"
-                        ></line>
-                        <line 
-                              x1="505"
-                              y1="125"
-                              x2="410"
-                              y2="220"
-                              height="12"
-                              stroke="black"
-                              stroke-width="3"
-                        ></line>
-                        <line 
-                              x1="510"
-                              y1="125"
-                              x2="615"
-                              y2="220"
-                              height="12"
-                              stroke="black"
-                              stroke-width="3"
-                        ></line>
-                        <line 
-                              x1="705"
-                              y1="125"
-                              x2="615"
-                              y2="220"
-                              height="12"
-                              stroke="black"
-                              stroke-width="3"
-                        ></line>
-                        <line 
-                              x1="710"
-                              y1="125"
-                              x2="815"
-                              y2="220"
-                              height="12"
-                              stroke="black"
-                              stroke-width="3"
-                        ></line>
-                        <line 
-                              x1="910"
-                              y1="125"
-                              x2="815"
-                              y2="220"
-                              height="12"
-                              stroke="black"
-                              stroke-width="3"
-                        ></line>
-                        <line 
-                              x1="910"
-                              y1="125"
-                              x2="1015"
-                              y2="220"
-                              height="12"
-                              stroke="black"
-                              stroke-width="3"
-                        ></line>
-                        <line 
-                              x1="1110"
-                              y1="125"
-                              x2="1015"
-                              y2="220"
-                              height="12"
-                              stroke="black"
-                              stroke-width="3"
-                        ></line>
-                        <line 
-                              x1="1110"
-                              y1="125"
-                              x2="1215"
-                              y2="220"
-                              height="12"
-                              stroke="black"
-                              stroke-width="3"
-                        ></line>
-                        <line 
-                              x1="1310"
-                              y1="125"
-                              x2="1215"
-                              y2="220"
-                              height="12"
-                              stroke="black"
-                              stroke-width="3"
-                        ></line>
-                        <line 
-                              x1="1310"
-                              y1="125"
-                              x2="1415"
-                              y2="220"
-                              height="12"
-                              stroke="black"
-                              stroke-width="3"
-                        ></line>
+                    <g>
+                       <polyline 
+                       fill="none"
+                       stroke="black"
+                       stroke-width="4"
+                       points="0,240 110,125 215,220 310,125 680,480"
+                       ></polyline>
+                      <polyline 
+                       fill="none"
+                       stroke="black"
+                       stroke-width="4"
+                       points="170,485 510,125 690,295 "
+                       ></polyline>
+                      <polyline 
+                       fill="none"
+                       stroke="black"
+                       stroke-width="4"
+                       points="565,285 712,125 845,255 "
+                       ></polyline>
+                      <polyline 
+                       fill="none"
+                       stroke="black"
+                       stroke-width="4"
+                       points="820,225 910,125 1035,245 "
+                       ></polyline>
+                      <polyline 
+                       fill="none"
+                       stroke="black"
+                       stroke-width="4"
+                       points="1005, 235 1110,125 1425,445"
+                       ></polyline>
+                      <polyline 
+                       fill="none"
+                       stroke="black"
+                       stroke-width="4"
+                       points="1205,245 1310,125 1385,195"
+                       ></polyline>
+                    </g>
                     </g>
                     <g>
                         <polygon
@@ -164,15 +76,14 @@ class Footer extends React.Component{
                           points=" 180,220 40,220, 110,155 "
                         />
                         <polygon
-                          fill="Peru
-                          "
+                          fill="Teal"
                           stroke="#000000"
                           stroke-width="2"
                           stroke-miterlimit="20"
                           points=" 380,220 245,220, 310,155 "
                         />
                         <polygon
-                          fill="IndianRed"
+                          fill="Teal"
                           stroke="#000000"
                           stroke-width="2"
                           stroke-miterlimit="20"
@@ -207,56 +118,113 @@ class Footer extends React.Component{
                           points=" 1380,220 1245,220, 1310,155 "
                         />
                     </g>
+                    <g>
+                      <polygon
+                          fill="Teal"
+                          stroke="#000000"
+                          stroke-width="2"
+                          stroke-miterlimit="20"
+                          points="90,130 -135,130, 15,205"
+                        />
+                      <polygon
+                          fill="Teal"
+                          stroke="#000000"
+                          stroke-width="2"
+                          stroke-miterlimit="20"
+                          points=" 290,130 135,130, 215,205 "
+                        />
+                      <polygon
+                          fill="Teal"
+                          stroke="#000000"
+                          stroke-width="2"
+                          stroke-miterlimit="20"
+                          points=" 490,130 335,130, 415,205 "
+                        />
+                      <polygon
+                          fill="Teal"
+                          stroke="#000000"
+                          stroke-width="2"
+                          stroke-miterlimit="20"
+                          points=" 690,130 535,130, 615,205 "
+                        />
+                      <polygon
+                          fill="Teal"
+                          stroke="#000000"
+                          stroke-width="2"
+                          stroke-miterlimit="20"
+                          points=" 890,130 735,130, 815,205 "
+                        />
+                      <polygon
+                          fill="Teal"
+                          stroke="#000000"
+                          stroke-width="2"
+                          stroke-miterlimit="20"
+                          points=" 1090,130 935,130, 1015,205 "
+                        />
+                      <polygon
+                          fill="Teal"
+                          stroke="#000000"
+                          stroke-width="2"
+                          stroke-miterlimit="20"
+                          points=" 1290,130 1135,130, 1215,205 "
+                        />
+                      <polygon
+                          fill="Teal"
+                          stroke="#000000"
+                          stroke-width="2"
+                          stroke-miterlimit="20"
+                          points=" 1490,130 1335,130, 1415,205 "
+                        />
+                    </g>
                     <g className="upper-Triangles"> 
                         <polygon
-                          fill="Teal"
+                          fill="Brown"
                           stroke="#000000"
                           stroke-width="2"
                           stroke-miterlimit="20"
-                          points=" 180,220 40,220, 110,155 "
+                          points=" 150,220 70,220, 110,185 "
                         />
                         <polygon
-                          fill="Peru
-                          "
+                          fill="Black"
                           stroke="#000000"
                           stroke-width="2"
                           stroke-miterlimit="20"
-                          points=" 380,220 245,220, 310,155 "
+                          points=" 345,220 275,220, 310,185 "
                         />
                         <polygon
-                          fill="IndianRed"
+                          fill="Sienna"
                           stroke="#000000"
                           stroke-width="2"
                           stroke-miterlimit="20"
-                          points=" 580,220 445,220, 510,155 "
+                          points=" 545,220 475,220, 510,185 "
                         />
                         <polygon
-                          fill="Teal"
+                          fill="Brown"
                           stroke="#000000"
                           stroke-width="2"
                           stroke-miterlimit="20"
-                          points=" 780,220 645,220, 710,155 "
+                          points=" 750,220 675,220, 710,185 "
                         />
                         <polygon
-                          fill="Teal"
+                          fill="Black"
                           stroke="#000000"
                           stroke-width="2"
                           stroke-miterlimit="20"
-                          points=" 980,220 845,220, 910,155 "
+                          points=" 950,220 875,220, 910,185 "
                         />
                         <polygon
-                          fill="Teal"
+                          fill="Brown"
                           stroke="#000000"
                           stroke-width="2"
                           stroke-miterlimit="20"
-                          points=" 1180,220 1045,220, 1110,155 "
+                          points=" 1150,220 1075,220, 1110,185 "
                         />
                         <polygon
-                          fill="Teal"
+                          fill="Black"
                           stroke="#000000"
                           stroke-width="2"
                           stroke-miterlimit="20"
-                          points=" 1380,220 1245,220, 1310,155 "
+                          points=" 1340,220 1285,220, 1310,185 "
                         />
                     </g>
                 </svg>
